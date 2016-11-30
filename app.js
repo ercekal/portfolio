8257f6b770b76b52be55b95114c6ffb135757fd5
ercekal <ercekalabalikoglu@gmail.com>
2016-11-30 23:44:23 +0000

    deletes user

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var works = require('./routes/works');
 var works = require('./routes/travel');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/works', works);
 app.use('/travel', travel);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
